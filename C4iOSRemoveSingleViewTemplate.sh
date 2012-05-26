d4f42a945b662f11c99b1b97074960a3c50f198c
Travis <travis@cocoaforartists.org>
2012-05-26 00:00:24 -0600

    First commit
    working installer for latest hacks into C4iOS from the C4 Summer
    Residency

---
+++ C4iOSRemoveSingleViewTemplate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  C4iOSRemoveSingleViewTemplate.sh
+#  
+#
+#  Created by Travis Kirton on 12-05-25.
+#  Copyright (c) 2012 POSTFL. All rights reserved.
+
+rm -f -d -r /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/Application/C4\ Single\ View\ Application.xctemplate
