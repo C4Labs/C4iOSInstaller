c674395c560f7c00485dbe6724ce2d3dc0a2504d
Travis <code@c4ios.com>
2013-02-07 07:14:25 -0700

    Pushing before creating new installer.
    Adding old files from July 2012.

---
+++ C4iOSMoveDocset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  C4iOSMoveDocset.sh
+#  
+#
+#  Created by Travis Kirton on 12-05-25.
+#  Copyright (c) 2012 POSTFL. All rights reserved.
+
+sudo mv /Users/com.C4iOS.C4.docset /Users/$USER/Library/Developer/Shared/Documentation/DocSets
--- C4iOSRemoveDocset.sh
+++ C4iOSRemoveDocset.sh
@@ -6,5 +6,5 @@
 #  Created by Travis Kirton on 12-05-25.
 #  Copyright (c) 2012 POSTFL. All rights reserved.
 
-rm -f -d -r /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Documentation/DocSets/org.cocoaforartists.C4.docset
+rm -f -d -r /Users/$USER/Library/Developer/Shared/Documentation/DocSets/com.C4iOS.C4.docset
 
