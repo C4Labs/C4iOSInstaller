87f6441324370dd8e2a08d7d01b01af8c407aa68
Travis <code@c4ios.com>
2013-05-28 13:14:34 -0700

    Some minor changes
    Updated the installer package project for the June 2013 release, made
    the C4iOSMoveDocset shell script more robust

--- C4iOSMoveDocset.sh
+++ C4iOSMoveDocset.sh
@@ -6,4 +6,8 @@
 #  Created by Travis Kirton on 12-05-25.
 #  Copyright (c) 2012 POSTFL. All rights reserved.
 
+[ ! -d /Users/$USER/Library/Developer ] && sudo mkdir /Users/$USER/Library/Developer || echo "exists"
+[ ! -d /Users/$USER/Library/Developer/Shared ] && sudo mkdir /Users/$USER/Library/Developer/Shared || echo "exists"
+[ ! -d /Users/$USER/Library/Developer/Shared/Documentation ] && sudo mkdir /Users/$USER/Library/Developer/Shared/Documentation || echo "exists"
+[ ! -d /Users/$USER/Library/Developer/Shared/Documentation/DocSets ] && sudo mkdir /Users/$USER/Library/Developer/Shared/Documentation/DocSets || echo "exists"
 sudo mv /Users/com.C4iOS.C4.docset /Users/$USER/Library/Developer/Shared/Documentation/DocSets
