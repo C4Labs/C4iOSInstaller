744bcdea4d4fa193a44fdab836bada813459b34f
-- C4 -- <travis@c4ios.com>
2014-06-03 18:33:22 -0700

    Modifying templates
    Adding file templates, splitting into file / project templates

--- buildPackageComponents.sh
+++ buildPackageComponents.sh
@@ -6,13 +6,12 @@
 #Single View Application 
 INSTALLER_VERSION="$1"
 
-TEMPLATES_VERSION="$1"
-TEMPLATES_LOCATION="./templates"
-TEMPLATES_NAME="INSTALL_TEMPLATES_$TEMPLATES_VERSION.pkg"
+PROJECT_TEMPLATES_VERSION="$1"
+PROJECT_TEMPLATES_LOCATION="./projectTemplates"
+PROJECT_TEMPLATES_NAME="INSTALL_TEMPLATES_$TEMPLATES_VERSION.pkg"
 #weird... using the TEMPLATES_DESTINATION variable doesn't work, but pasting it in does!
 #TEMPLATES_DESTINATION="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/Application"
-
-pkgbuild --root $TEMPLATES_LOCATION --identifier com.c4ios.installer.templates --version $INSTALLER_VERSION --install-location /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/Application $TEMPLATES_NAME
+pkgbuild --root $PROJECT_TEMPLATES_LOCATION --identifier com.c4ios.installer.templates --version $INSTALLER_VERSION --install-location /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/Application $PROJECT_TEMPLATES_NAME
 
 DOCSET_VERSION="$1"
 DOCSET_LOCATION="./docs"
@@ -20,3 +19,4 @@ DOCSET_NAME="INSTALL_DOCSET_$DOCSET_VERSION.pkg"
 #weird... using the DOCSET_DESTINATION variable doesn't work, but pasting it in does!
 #DOCSET_DESTINATION="/Users/$USER/Desktop/Install/DocSets"
 pkgbuild --root "$DOCSET_LOCATION" --identifier com.c4ios.installer.docset --version $INSTALLER_VERSION --install-location /Users/$USER/Library/Developer/Shared/Documentation/DocSets/ $DOCSET_NAME
+
