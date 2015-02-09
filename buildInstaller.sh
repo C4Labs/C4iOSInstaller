beebcbfaaf46bd8e54740ec38aaa5aa3411dda25
-- C4 -- <info@c4ios.com>
2015-02-08 23:32:26 -0800

    New ObjC-codebase Installer for Xcode6
    Replaces non-functional version as of Feb 2015.
    
    Can be modified to include optional Swift version at later date.

--- buildInstaller.sh
+++ buildInstaller.sh
@@ -1,4 +1,4 @@
-#  buildInstaller.sh
+#  buildIn/Users/travis/Development/C4iOSInstaller/buildInstaller.shstaller.sh
 #
 #  Created by Travis Kirton on 14-06-02.
 
@@ -10,10 +10,12 @@ sh buildPackageComponents.sh $VERSION
 
 # defines the names of the component packages, this matches their names defined in the buildPackageComponents.sh script
 TEMPLATES_PKG="INSTALL_TEMPLATES_0_02.pkg"
+IOSBASE_PKG="INSTALL_IOS_BASE_0_01.pkg"
+APPBASE_PKG="INSTALL_APP_BASE_0_01.pkg"
 DOCSET_PKG="INSTALL_DOCSET_0_01.pkg"
 
 #builds the distribution xml
-productbuild --synthesize --package $TEMPLATES_PKG --package $DOCSET_PKG distribution.xml
+productbuild --synthesize --package $TEMPLATES_PKG --package $IOSBASE_PKG --package $APPBASE_PKG --package $DOCSET_PKG distribution.xml
 
 #modifies the distribution 
 sh updateDistributionXML.sh
@@ -28,5 +30,7 @@ productsign --sign "Developer ID Installer: Travis Stuart Kirton (75C7KVJZ99)" u
 mv signed.pkg C4Installer_$VERSION.pkg
 
 rm -Rf $TEMPLATES_PKG
+rm -Rf $IOSBASE_PKG
+rm -Rf $APPBASE_PKG
 rm -Rf $DOCSET_PKG
 rm -Rf unsigned.pkg
--- buildPackageComponents.sh
+++ buildPackageComponents.sh
@@ -13,6 +13,16 @@ PROJECT_TEMPLATES_NAME="INSTALL_TEMPLATES_0_02.pkg"
 #TEMPLATES_DESTINATION="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/Application"
 pkgbuild --root $PROJECT_TEMPLATES_LOCATION --identifier com.c4ios.installer.templates --version $INSTALLER_VERSION --install-location /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/iOS/Application $PROJECT_TEMPLATES_NAME
 
+IOS_BASE_VERSION="$1"
+IOS_BASE_LOCATION="./iosBase"
+IOS_BASE_NAME="INSTALL_IOS_BASE_0_01.pkg"
+pkgbuild --root $IOS_BASE_LOCATION --identifier com.c4ios.installer.iosbase --version $INSTALLER_VERSION --install-location /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project\ Templates/iOS $IOS_BASE_NAME
+
+APP_BASE_VERSION="$1"
+APP_BASE_LOCATION="./base"
+APP_BASE_NAME="INSTALL_APP_BASE_0_01.pkg"
+pkgbuild --root $APP_BASE_LOCATION --identifier com.c4ios.installer.appbase --version $INSTALLER_VERSION --install-location /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Base $APP_BASE_NAME
+
 DOCSET_VERSION="$1"
 DOCSET_LOCATION="./docs"
 DOCSET_NAME="INSTALL_DOCSET_0_01.pkg"
